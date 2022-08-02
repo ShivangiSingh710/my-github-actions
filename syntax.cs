16a21238dcd03599f87f2ab0ee3a6aea58a9d8d2
ShivangiSingh710 <110090863+ShivangiSingh710@users.noreply.github.com>
2022-08-02 12:14:43 +0530

    Updating the changes suggested by workflow

--- syntax.cs
+++ syntax.cs
@@ -5,15 +5,15 @@ using System.Text;
 using System.Threading.Tasks;
 namespace ConsoleApp2
 {
-   class Program
-   {
-       static void Main(string[] args)
-       {
-           int a, b, c;
-           a = 10; b = 20;
-            sum = a + b;
-           Console.WriteLine("sum of a and b = "+c);
-        }
-    }
+     class Program
+     {
+         static void Main(string[] args)
+         {
+             int a, b, c;
+             a = 10; b = 20;
+             sum = a + b;
+             Console.WriteLine("sum of a and b = " + c);
+         }
+     }
 }
 
