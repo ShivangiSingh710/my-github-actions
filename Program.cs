ff7aa26a2a7dfb57f88a69869d113237ade914c6
ShivangiSingh710 <110090863+ShivangiSingh710@users.noreply.github.com>
2022-08-02 13:00:57 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,16 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-namespace ConsoleApp2
-{
+
  class Program
      {
          static void Main(string[] args)
          {
-             int a, b, c;
-             a = 10; b = 20;
-             sum = a + b;
-             Console.WriteLine("sum of a and b = " + c);
+            Console.WriteLine("Super-Linter Check");
          }
      }
 }
