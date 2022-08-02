be8d6cc545d2f052ee7e230bf2184de5e3c2cbe2
ShivangiSingh710 <110090863+ShivangiSingh710@users.noreply.github.com>
2022-08-02 11:37:56 +0530

    Create syntax.cs

---
+++ syntax.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            a = 10; b = 20; 
+            sum = a + b;
+            Console.WriteLine("sum of a and b = "+c);
+        }
+    }
+}
+
